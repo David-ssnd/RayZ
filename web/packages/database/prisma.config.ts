c266bd98f2a076fdf0bb6e4da42636f509b8300b
David <david.krivoklatsky@student.ssnd.sk>
2026-01-24 13:33:50 +0100

    Fix: DB .env added, target platformio updated for S3 supermini support

--- web/packages/database/prisma.config.ts
+++ web/packages/database/prisma.config.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import dotenv from 'dotenv'
 import { defineConfig } from 'prisma/config'
 
-dotenv.config({ path: path.resolve(process.cwd(), '../../.env') })
+dotenv.config({ path: path.resolve(process.cwd(), '.env') })
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
